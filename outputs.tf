75da8e366b734ffa88f3659f1e710121fbf32f6e
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "automation_connections" {
   description = "All automation_connection resources"
   value       = azurerm_automation_connection.automation_connections
 }
+output "automation_connections_automation_account_name" {
+  description = "List of automation_account_name values across all automation_connections"
+  value       = [for k, v in azurerm_automation_connection.automation_connections : v.automation_account_name]
+}
+output "automation_connections_description" {
+  description = "List of description values across all automation_connections"
+  value       = [for k, v in azurerm_automation_connection.automation_connections : v.description]
+}
+output "automation_connections_name" {
+  description = "List of name values across all automation_connections"
+  value       = [for k, v in azurerm_automation_connection.automation_connections : v.name]
+}
+output "automation_connections_resource_group_name" {
+  description = "List of resource_group_name values across all automation_connections"
+  value       = [for k, v in azurerm_automation_connection.automation_connections : v.resource_group_name]
+}
+output "automation_connections_type" {
+  description = "List of type values across all automation_connections"
+  value       = [for k, v in azurerm_automation_connection.automation_connections : v.type]
+}
+output "automation_connections_values" {
+  description = "List of values values across all automation_connections"
+  value       = [for k, v in azurerm_automation_connection.automation_connections : v.values]
+}
 
